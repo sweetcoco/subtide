8d0771e5fe501dcf25ae1659ededc911089b93ed
Corey Howell <cjthowell@gmail.com>
2015-06-10 11:08:35 -0400

    Updated subtide

--- Brocfile.js
+++ Brocfile.js
@@ -6,6 +6,8 @@ var app = new EmberApp();
 
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+    this.route('settings', { path: '/settings'} );
 });
 
 export default Router;
