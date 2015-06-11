27aa624dfc31be99cc0ffead08bbe62e679e065e
Corey Howell <cjthowell@gmail.com>
2015-06-10 20:16:43 -0400

    adjusted the namespacing - right now it's just defaults sitting in the head of the index.html. Since this file never gets copied to the server/dist it works just fine for now.

---
+++ app/controllers/settings.js
@@ -0,0 +1,10 @@
+export default Ember.Controller.extend({
+  // the initial value of the `search` property
+  email: appStrap.email,
+  username: appStrap.username,
+  firstName: appStrap.firstName,
+  lastName: appStrap.lastName,
+
+  actions: {
+  }
+});
